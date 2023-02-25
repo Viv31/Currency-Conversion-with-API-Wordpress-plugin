b345f2cd20462cfcbe4f141ca636faded7a012c8
vaibhav <vivgangs@gmail.com>
2023-02-25 23:30:51 +0530

    Add prefex for the function and update BS & JS Library

--- libraries.php
+++ libraries.php
@@ -14,8 +14,8 @@ if (!function_exists('CCC__load_admin_js'))
 }
 
 /* Include CSS and Script */
-   add_action('wp_enqueue_scripts','plugin_css_jsscripts');
-   function plugin_css_jsscripts() {
+   add_action('wp_enqueue_scripts','CCC__plugin_css_jsscripts');
+   function CCC__plugin_css_jsscripts() {
       // CSS
       wp_enqueue_style( 'style-css', plugins_url( '/style.css', __FILE__ ));
    
@@ -36,11 +36,11 @@ if (!function_exists('CCC__register_script'))
     {
         wp_register_script('bootstrap_js', plugins_url('assets/js/bootstrap.min.js', __FILE__) , array(
             'jquery'
-        ) , '4.6.0');
+        ) , '5.2.3');
         wp_register_script('custom_js', plugins_url('assets/js/script.js', __FILE__) , array(
             'CustonJquery'
         ) , '4.6.0');
-        wp_register_style('boostrap_css', plugins_url('assets/css/bootstrap-min.css', __FILE__) , false, '4.6.0', 'all');
+        wp_register_style('boostrap_css', plugins_url('assets/css/bootstrap-min.css', __FILE__) , false, '5.2.3', 'all');
         wp_register_style('custom_css', plugins_url('assets/css/style.css', __FILE__) , false, '1.0.0', 'all');
     }
 
